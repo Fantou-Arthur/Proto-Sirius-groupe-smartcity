509d979748a82082377d7ead46c9e13d346e4e8d
onecode24 <angelo24yanve@gmail.com>
2025-03-21 12:36:50 +0100

    feat: merge update develop to feat/user

--- xmart-frontend/src/main/java/edu/ezip/ing1/pds/controllers/place/ListPlacesController.java
+++ xmart-frontend/src/main/java/edu/ezip/ing1/pds/controllers/place/ListPlacesController.java
@@ -62,8 +62,8 @@ public class ListPlacesController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
         placeListTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        hboxList.setHgrow(placeListTable, javafx.scene.layout.Priority.ALWAYS);
         nameColumn.setCellValueFactory(new PropertyValueFactory<PlaceCell, String>("name"));
         addressColumn.setCellValueFactory(new PropertyValueFactory<PlaceCell, String>("address"));
         capacityColumn.setCellValueFactory(new PropertyValueFactory<PlaceCell, Integer>("maxCapacity"));
